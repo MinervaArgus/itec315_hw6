15f63b946d2892d3c98d472e929088298e2232a3
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2023-03-24 23:00:31 -0400

    Added some AudioPlayerFunctionality

--- AudioPlayer/Form1.Designer.cs
+++ AudioPlayer/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            openFileBtn = new Button();
+            SuspendLayout();
+            // 
+            // openFileBtn
+            // 
+            openFileBtn.BackColor = SystemColors.ControlLight;
+            openFileBtn.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            openFileBtn.Location = new Point(1017, 41);
+            openFileBtn.Name = "openFileBtn";
+            openFileBtn.Size = new Size(191, 59);
+            openFileBtn.TabIndex = 0;
+            openFileBtn.Text = "📂 Open File";
+            openFileBtn.UseVisualStyleBackColor = false;
+            openFileBtn.Click += openFileBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(1262, 673);
+            Controls.Add(openFileBtn);
+            MaximumSize = new Size(1280, 720);
+            MinimumSize = new Size(1280, 720);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button openFileBtn;
     }
 }
--- AudioPlayer/Form1.cs
+++ AudioPlayer/Form1.cs
@@ -1,10 +1,25 @@
 namespace AudioPlayer
 {
+    using NAudio.Wave;
+    using NetCoreAudio; //Allows the media to be played
+    using System; // Need this for every app
+    using System.Drawing;
+    using System.Windows.Forms;
+    using System.IO;
+    using System.ComponentModel;
+    using TagLib;
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
