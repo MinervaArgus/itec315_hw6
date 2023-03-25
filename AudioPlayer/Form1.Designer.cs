6633cbab9cb9f9dc1ff25e4f18812dba30e437a0
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2023-03-25 00:35:03 -0400

    additional update to UI and code

--- AudioPlayer/Form1.Designer.cs
+++ AudioPlayer/Form1.Designer.cs
@@ -29,19 +29,110 @@
         private void InitializeComponent()
         {
             openFileBtn = new Button();
+            pictureBoxAlbum = new PictureBox();
+            rewindBtn = new Button();
+            pauseBtn = new Button();
+            playBtn = new Button();
+            fastForwardBtn = new Button();
+            stopBtn = new Button();
+            songTitleLabel = new Label();
+            progressBar = new ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAlbum).BeginInit();
             SuspendLayout();
             // 
             // openFileBtn
             // 
             openFileBtn.BackColor = SystemColors.ControlLight;
             openFileBtn.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            openFileBtn.Location = new Point(1017, 41);
+            openFileBtn.Location = new Point(41, 138);
             openFileBtn.Name = "openFileBtn";
-            openFileBtn.Size = new Size(191, 59);
+            openFileBtn.Size = new Size(240, 59);
             openFileBtn.TabIndex = 0;
-            openFileBtn.Text = "📂 Open File";
+            openFileBtn.Text = "📂 Open Media File";
             openFileBtn.UseVisualStyleBackColor = false;
             openFileBtn.Click += openFileBtn_Click;
+            // 
+            // pictureBoxAlbum
+            // 
+            pictureBoxAlbum.Location = new Point(41, 253);
+            pictureBoxAlbum.MaximumSize = new Size(240, 240);
+            pictureBoxAlbum.MinimumSize = new Size(240, 240);
+            pictureBoxAlbum.Name = "pictureBoxAlbum";
+            pictureBoxAlbum.Size = new Size(240, 240);
+            pictureBoxAlbum.TabIndex = 1;
+            pictureBoxAlbum.TabStop = false;
+            // 
+            // rewindBtn
+            // 
+            rewindBtn.BackColor = Color.Transparent;
+            rewindBtn.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rewindBtn.Location = new Point(362, 492);
+            rewindBtn.Name = "rewindBtn";
+            rewindBtn.Size = new Size(147, 41);
+            rewindBtn.TabIndex = 2;
+            rewindBtn.Text = "<<";
+            rewindBtn.UseVisualStyleBackColor = false;
+            // 
+            // pauseBtn
+            // 
+            pauseBtn.BackColor = Color.Transparent;
+            pauseBtn.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            pauseBtn.Location = new Point(515, 492);
+            pauseBtn.Name = "pauseBtn";
+            pauseBtn.Size = new Size(147, 41);
+            pauseBtn.TabIndex = 3;
+            pauseBtn.Text = "||";
+            pauseBtn.UseVisualStyleBackColor = false;
+            // 
+            // playBtn
+            // 
+            playBtn.BackColor = Color.Transparent;
+            playBtn.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            playBtn.Location = new Point(668, 492);
+            playBtn.Name = "playBtn";
+            playBtn.Size = new Size(147, 41);
+            playBtn.TabIndex = 4;
+            playBtn.Text = "▶";
+            playBtn.UseVisualStyleBackColor = false;
+            // 
+            // fastForwardBtn
+            // 
+            fastForwardBtn.BackColor = Color.Transparent;
+            fastForwardBtn.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            fastForwardBtn.Location = new Point(821, 492);
+            fastForwardBtn.Name = "fastForwardBtn";
+            fastForwardBtn.Size = new Size(147, 41);
+            fastForwardBtn.TabIndex = 5;
+            fastForwardBtn.Text = ">>";
+            fastForwardBtn.UseVisualStyleBackColor = false;
+            // 
+            // stopBtn
+            // 
+            stopBtn.BackColor = Color.Transparent;
+            stopBtn.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            stopBtn.Location = new Point(974, 492);
+            stopBtn.Name = "stopBtn";
+            stopBtn.Size = new Size(147, 41);
+            stopBtn.TabIndex = 6;
+            stopBtn.Text = "◼️";
+            stopBtn.UseVisualStyleBackColor = false;
+            // 
+            // songTitleLabel
+            // 
+            songTitleLabel.AutoSize = true;
+            songTitleLabel.BackColor = Color.Transparent;
+            songTitleLabel.Font = new Font("Consolas", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            songTitleLabel.Location = new Point(362, 253);
+            songTitleLabel.Name = "songTitleLabel";
+            songTitleLabel.Size = new Size(0, 27);
+            songTitleLabel.TabIndex = 7;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(362, 455);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(759, 17);
+            progressBar.TabIndex = 8;
             // 
             // Form1
             // 
@@ -49,16 +140,34 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             ClientSize = new Size(1262, 673);
+            Controls.Add(progressBar);
+            Controls.Add(songTitleLabel);
+            Controls.Add(stopBtn);
+            Controls.Add(fastForwardBtn);
+            Controls.Add(playBtn);
+            Controls.Add(pauseBtn);
+            Controls.Add(rewindBtn);
+            Controls.Add(pictureBoxAlbum);
             Controls.Add(openFileBtn);
             MaximumSize = new Size(1280, 720);
             MinimumSize = new Size(1280, 720);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAlbum).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button openFileBtn;
+        private PictureBox pictureBoxAlbum;
+        private Button rewindBtn;
+        private Button pauseBtn;
+        private Button playBtn;
+        private Button fastForwardBtn;
+        private Button stopBtn;
+        private Label songTitleLabel;
+        private ProgressBar progressBar;
     }
 }
--- AudioPlayer/Form1.cs
+++ AudioPlayer/Form1.cs
@@ -8,18 +8,107 @@ namespace AudioPlayer
     using System.IO;
     using System.ComponentModel;
     using TagLib;
-
+    using NAudio.Wave.SampleProviders;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
     public partial class Form1 : Form
     {
+        private WaveOutEvent waveOut;
+        private IWaveProvider audioFileReader;
+        private Player player;
+        private string currentFilePath;
+        private Timer timer;
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Initializes the Audio Player and assigns all the instance variables to their values
+        private void IntializeAudioPlayer()
+        {
+            player = new Player();
+            waveOut = new WaveOutEvent();
+            waveOut.PlaybackStopped += OnPlaybackStopped;
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += UpdateProgressBar;
+        }
+
+        private void OnPlaybackStopped(object sender, EventArgs e)
+        {
+            UpdateUIForStoppedPlayback();
+        }
+
+        private void UpdateUIForStoppedPlayback()
+        {
+            playBtn.Enabled = true;
+            pauseBtn.Enabled = false;
+            stopBtn.Enabled = false;
+            rewindBtn.Enabled = false;
+            fastForwardBtn.Enabled = false;
+            progressBar.Value = 0;
+
+        }
+
         private void openFileBtn_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Audio Files| *.mp3;*.wav"; // Filters for 2 of the 7 File Formats as required
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = openFileDialog.FileName;
+                LoadAudioFile();
+                UpdateUIForNewFile();
+            }
+        }
+
+        // Loads the provided audio file 
+        private void LoadAudioFile()
+        {
+            if (audioFileReader != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = new WaveOutEvent();
+                waveOut.PlaybackStopped += OnPlaybackStopped;
+            }
+
+            using (var tempReader = new AudioFileReader(currentFilePath))
+            {
+                audioFileReader = new SampleToWaveProvider(tempReader);
+                waveOut.Init(audioFileReader);
+            }
+
+            var tagFile = TagLib.File.Create(currentFilePath);
+            songTitleLabel.Text = tagFile.Tag.Title ?? Path.GetFileNameWithoutExtension(currentFilePath);
+
+            if (tagFile.Tag.Pictures.Length > 0)
+            {
+                var albumImage = tagFile.Tag.Pictures[0];
+                using (var stream = new MemoryStream(albumImage.Data.Data))
+                {
+                    pictureBoxAlbum.Image = Image.FromStream(stream);
+                }
+            }
+            else
+            {
+                pictureBoxAlbum.Image = null;
+            }
+        }
+
+        private void UpdateUIForNewFile()
+        {
+            playBtn.Enabled = true; // Enable Play btn for new song
+            pauseBtn.Enabled = true; // Enable Pause btn for new song
+            stopBtn.Enabled = true; // Enable Stop btn for new song
+            rewindBtn.Enabled = true; // Enable Rewind btn for new song
+            fastForwardBtn.Enabled = true; // Enable Fast Forward btn for new song
+            progressBar.Value = 0; // Set the Progress Bar to 0 for new song
+
+            // Update the title of the song
+            songTitleLabel.Text = Path.GetFileNameWithoutExtension(currentFilePath);
         }
     }
 }
